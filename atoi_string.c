b5487920bb2a8bca7c98274c51e2635b642c6304
mind1 <ddong911201@gmail.com>
2015-05-06 00:23:24 +0900

    Update atoi_string.c

--- atoi_string.c
+++ atoi_string.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 
 int main(int argc, void* argv[]){
-	printf(" String : %s\n",(char *)argv[1]);
-	printf(" Integer : %d\n",atoi((char *)argv[1]));
+	printf(" String : %s\n",(char *)argv[1]);		//argv[1]'s Character
+	printf(" Integer : %d\n",atoi((char *)argv[1]));	//argv[1]'s Integer
+	
 	return 0;
 }
